3f86fda8acca6ffe5f6995cdcf5c3fc689c60c6e
PerSolberg <persolberg@hotmail.com>
2016-02-03 23:12:53 +0100

    Synkronisering (litt mer lærdom om alarmer).

--- app/src/main/java/no/shitt/myshit/AlarmReceiver.java
+++ app/src/main/java/no/shitt/myshit/AlarmReceiver.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -41,6 +42,10 @@ public class AlarmReceiver extends WakefulBroadcastReceiver {
          * This intent holds an extra identifying the wake lock.
          */
         Intent service = new Intent(context, SchedulingService.class);
+        service.setData(intent.getData());
+        service.putExtras(intent.getExtras());
+        Log.d("AlarmReceiver", "Received intent: " + intent.describeContents());
+        Log.d("AlarmReceiver", "Action: " + intent.getAction() + ", Data: " + intent.getData());
 
         // Start the service, keeping the device awake while it is launching.
         startWakefulService(context, service);
